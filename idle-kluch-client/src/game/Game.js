4d00d0367a3b92a7fceb429d4e1cfe13e025f0bb
--global <piknypiec@gmail.com>
2018-03-24 19:20:43 +0100

    Small refactor

--- idle-kluch-client/src/game/Game.js
+++ idle-kluch-client/src/game/Game.js
@@ -11,8 +11,25 @@ const createGame = () => {
   let game = null;
   // let background = null;
 
-  const gameUpdate = () => {
-    // game.world.setBounds
+  const preload = function() {
+	console.log('preloading!')
+	this.load.image('grass_1', 'grass_1.png');
+  };
+
+  const create = function() {
+	console.log('creating!');
+
+	const tileMap = getTiles();
+	const tiles = Object.values(tileMap);
+	for(let i = 0; i < tiles.length; i++) {
+	  const tile = tiles[i];
+	  const { x, y } = tile;
+	  const sprite = this.add.sprite(x * TILE_SIZE, y * TILE_SIZE, 'grass_1');
+	}
+  };
+
+  const update = () => {
+	// game.world.setBounds
 	// console.log('rendering')
 	// game(game.camera, 32, 32);
   };
@@ -21,29 +38,16 @@ const createGame = () => {
 
   };
 
+
   const config = {
 	type: Phaser.CANVAS,
 	parent: 'game',
 	width: TILE_SIZE * 10,
 	height: TILE_SIZE * 6,
 	scene: {
-	  preload: function() {
-		console.log('preloading!')
-		this.load.image('grass_1', 'grass_1.png');
-	  },
-	  create: function() {
-		console.log('creating!');
-
-		const tileMap = getTiles();
-		const tiles = Object.values(tileMap);
-		for(let i = 0; i < tiles.length; i++) {
-		  const tile = tiles[i];
-		  const { x, y } = tile;
-		  const sprite = this.add.sprite(x * TILE_SIZE, y * TILE_SIZE, 'grass_1');
-		}
-
-	  },
-	  update: gameUpdate,
+	  preload,
+	  create,
+	  update,
 	  render,
 	}
   };
