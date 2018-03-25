9b1d186d13a4b713442f175aa32d4d937d0287bd
--global <piknypiec@gmail.com>
2018-03-25 16:52:57 +0200

    Basic game camera

--- idle-kluch-client/src/game/Game.js
+++ idle-kluch-client/src/game/Game.js
@@ -1,4 +1,4 @@
-import { getTiles as _getTiles} from './selectors';
+import { getTiles as _getTiles } from './selectors';
 import store from '../store/store';
 import Phaser from 'phaser';
 
@@ -9,29 +9,55 @@ const TILE_SIZE = 128;
 const createGame = () => {
 
   let game = null;
+  let camera = null;
+
+  let cursors = null;
   // let background = null;
 
-  const preload = function() {
+  const preload = function () {
 	console.log('preloading!')
 	this.load.image('grass_1', 'grass_1.png');
+	// this.camera.bounds = undefined;
   };
 
-  const create = function() {
+  const create = function () {
 	console.log('creating!');
 
 	const tileMap = getTiles();
 	const tiles = Object.values(tileMap);
-	for(let i = 0; i < tiles.length; i++) {
+	for (let i = 0; i < tiles.length; i++) {
 	  const tile = tiles[i];
 	  const { x, y } = tile;
 	  const sprite = this.add.sprite(x * TILE_SIZE, y * TILE_SIZE, 'grass_1');
 	}
+
+	cursors = game.input.keyboard.createCursorKeys();
+
+	game.world.resize(5000, 5000);
+	// game.world.setBounds(-2000, -2000, 2000, 2000);
+	game.camera.x = 0;
+	game.camera.y = 0;
   };
 
   const update = () => {
+	// console.log(game.camera.bounds);
+	// console.log(game.world.bounds);
 	// game.world.setBounds
 	// console.log('rendering')
 	// game(game.camera, 32, 32);
+	const {x, y} = game.camera;
+	if (cursors.up.isDown) {
+	  game.camera.y = y - 5;
+	}
+	if (cursors.down.isDown) {
+	  game.camera.y = y + 5;
+	}
+	if (cursors.left.isDown) {
+	  game.camera.x = x - 5;
+	}
+	if (cursors.right.isDown) {
+	  game.camera.x = x + 5;
+	}
   };
 
   const render = () => {
@@ -42,7 +68,7 @@ const createGame = () => {
   const config = {
 	type: Phaser.CANVAS,
 	parent: 'game',
-	width: TILE_SIZE * 10,
+	width: TILE_SIZE * 14,
 	height: TILE_SIZE * 6,
 	scene: {
 	  preload,
@@ -53,12 +79,17 @@ const createGame = () => {
   };
 
   game = new Phaser.Game(
-    config.width, config.height,
+	config.width, config.height,
 	config.type, config.parent,
 	{
 	  ...config.scene
 	}
   );
+
+  // Phaser.Camera.shake();
+
+  // camera = new Phaser.Camera(game, 0, 0, 0, config.width, config.height);
+  // camera.shake()
 
   return game;
 };
