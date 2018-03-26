cfefb68b58e718b5e4b21da0ae5316ab2c2d60ba
--global <piknypiec@gmail.com>
2018-03-26 11:36:34 +0200

    Load building descriptions from file

--- idle-kluch-server/src/main/java/com/soze/idlekluch/kingdom/service/BuildingServiceImpl.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/kingdom/service/BuildingServiceImpl.java
@@ -1,5 +1,9 @@
 package com.soze.idlekluch.kingdom.service;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.soze.idlekluch.utils.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,32 +12,38 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class BuildingServiceImpl implements BuildingService {
 
   private static final Logger LOG = LoggerFactory.getLogger(BuildingServiceImpl.class);
 
+  private final Map<String, Object> buildingDefinitions = new HashMap<>();
+
   @Value("buildings.json")
   private ClassPathResource buildings;
 
   @PostConstruct
-  public void setup() {
+  public void setup() throws IOException {
     LOG.info("Initializing building service.");
 
     //read buildings here
+    final String fileContent = readBuildings();
 
-    //create building descriptions here
+    //read it as a List of Maps
+    final ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+    buildingDefinitions.putAll(mapper.readValue(fileContent, new TypeReference<Map<String, Object>>(){}));
+    LOG.info("Read [{}] building definitions", buildingDefinitions.size());
+    //TODO ? create building descriptions here
 
     //then later create them, add to ECS, transform them etc.
+  }
 
-    System.out.println(buildings.toString());
-    try {
-      System.out.println(buildings.getFile().getAbsolutePath());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
+  private String readBuildings() {
+    return FileUtils.readClassPathResource(buildings);
   }
 
 }
---
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/utils/io/FileUtils.java
@@ -0,0 +1,45 @@
+package com.soze.idlekluch.utils.io;
+
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.*;
+import java.util.Objects;
+
+public class FileUtils {
+
+  public static String readFile(final File file) {
+    Objects.requireNonNull(file);
+
+
+    return "";
+  }
+
+  public static String readInputStream(final InputStream inputStream) {
+    final StringBuilder stringBuilder = new StringBuilder();
+
+    try (final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+      final int bufferSize = 1024;
+      final char[] buffer = new char[bufferSize];
+      int bytesRead;
+
+      while((bytesRead = br.read(buffer, 0, buffer.length)) > 0) {
+        stringBuilder.append(buffer, 0, bytesRead);
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      return stringBuilder.toString();
+    }
+  }
+
+  public static String readClassPathResource(final ClassPathResource classPathResource) {
+    try {
+      return readInputStream(classPathResource.getInputStream());
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+}
