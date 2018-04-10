798e76b5c89f93961518cd7b79ac5baf49e1ff74
sozemego <piknypiec@gmail.com>
2018-04-10 14:10:46 +0200

    Basic convert for building to entity e

--- idle-kluch-server/src/main/java/com/soze/idlekluch/game/engine/EntityConverter.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/game/engine/EntityConverter.java
@@ -2,7 +2,9 @@ package com.soze.idlekluch.game.engine;
 
 import com.soze.idlekluch.game.engine.components.PhysicsComponent;
 import com.soze.idlekluch.game.service.GameEngine;
+import com.soze.idlekluch.kingdom.dto.BuildingDefinitionDto;
 import com.soze.idlekluch.kingdom.entity.Building;
+import com.soze.idlekluch.kingdom.service.BuildingService;
 import com.soze.klecs.entity.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,10 +18,12 @@ import java.util.Objects;
 public class EntityConverter {
 
   private final GameEngine gameEngine;
+  private final BuildingService buildingService;
 
   @Autowired
-  public EntityConverter(GameEngine gameEngine) {
+  public EntityConverter(GameEngine gameEngine, BuildingService buildingService) {
     this.gameEngine = Objects.requireNonNull(gameEngine);
+    this.buildingService = Objects.requireNonNull(buildingService);
   }
 
   public Entity convert(final Building building) {
@@ -30,6 +34,11 @@ public class EntityConverter {
     final PhysicsComponent physicsComponent = new PhysicsComponent();
     physicsComponent.setX(building.getX());
     physicsComponent.setY(building.getY());
+
+    final BuildingDefinitionDto buildingDefinitionDto = buildingService.getAllConstructableBuildings().get(building.getDefinitionId());
+    physicsComponent.setWidth(buildingDefinitionDto.getWidth());
+    physicsComponent.setHeight(buildingDefinitionDto.getHeight());
+    entity.addComponent(physicsComponent);
 
     return entity;
   }
