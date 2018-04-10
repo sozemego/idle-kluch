227b533bf15854f01bb527af9f36af8ac35fd4a2
sozemego <piknypiec@gmail.com>
2018-04-10 14:43:09 +0200

    Nodes interface

---
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/game/engine/nodes/Nodes.java
@@ -0,0 +1,10 @@
+package com.soze.idlekluch.game.engine.nodes;
+
+import com.soze.idlekluch.game.engine.components.PhysicsComponent;
+import com.soze.klecs.node.Node;
+
+public interface Nodes {
+
+  Node PHYSICAL_NODE = Node.of(PhysicsComponent.class);
+
+}
