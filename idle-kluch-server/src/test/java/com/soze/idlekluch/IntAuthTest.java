2170ff2897ed357fa7509352678df515ad73f5cf
--global <piknypiec@gmail.com>
2018-04-22 19:26:03 +0200

    Basic game service int tests and base integration auth test

---
+++ idle-kluch-server/src/test/java/com/soze/idlekluch/IntAuthTest.java
@@ -0,0 +1,46 @@
+package com.soze.idlekluch;
+
+import com.soze.idlekluch.kingdom.dto.RegisterKingdomForm;
+import com.soze.idlekluch.kingdom.service.KingdomService;
+import com.soze.idlekluch.user.dto.RegisterUserForm;
+import com.soze.idlekluch.user.service.UserService;
+import com.soze.idlekluch.utils.CommonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
+
+/**
+ * Used for integration tests which require users to exist.
+ */
+public class IntAuthTest {
+
+  @Autowired
+  private UserService userService;
+  @Autowired
+  private KingdomService kingdomService;
+
+  private final char[] password = CommonUtils.generateRandomString(15).toCharArray();
+
+  /**
+   * Creates a user with given name. Does nothing if this
+   * user already exists.
+   */
+  protected void register(final String username) {
+    Objects.requireNonNull(username);
+    try {
+      userService.addUser(new RegisterUserForm(username, password));
+    } catch (Exception e) {
+
+    }
+  }
+
+  /**
+   * Creates a kingdom for given user.
+   * @param username
+   * @param kingdomName
+   */
+  protected void createKingdom(final String username, final String kingdomName) {
+    kingdomService.addKingdom(username, new RegisterKingdomForm(kingdomName));
+  }
+
+}
--- idle-kluch-server/src/test/java/com/soze/idlekluch/game/GameServiceIntTests.java
+++ idle-kluch-server/src/test/java/com/soze/idlekluch/game/GameServiceIntTests.java
@@ -1,21 +1,22 @@
 package com.soze.idlekluch.game;
 
+import com.soze.idlekluch.IntAuthTest;
 import com.soze.idlekluch.RootConfig;
-import com.soze.idlekluch.WebApplicationInitializer;
+import com.soze.idlekluch.game.message.BuildBuildingForm;
 import com.soze.idlekluch.game.message.WorldChunkMessage;
 import com.soze.idlekluch.game.service.GameService;
-import com.soze.idlekluch.game.service.WebSocketMessagingService;
 import com.soze.idlekluch.game.service.WebSocketMessagingServiceTest;
+import com.soze.idlekluch.kingdom.exception.UserDoesNotHaveKingdomException;
 import com.soze.idlekluch.routes.Routes;
+import com.soze.idlekluch.user.exception.AuthUserDoesNotExistException;
+import com.soze.idlekluch.utils.CommonUtils;
 import com.soze.idlekluch.utils.JsonUtils;
 import com.soze.idlekluch.utils.sql.DatabaseReset;
-import com.soze.idlekluch.world.service.World;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,18 +24,16 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(
   classes = {
     RootConfig.class
-//    WebApplicationInitializer.class
   }
 )
 @WebAppConfiguration
 @ActiveProfiles({"integration-test"})
-public class GameServiceIntTests {
+public class GameServiceIntTests extends IntAuthTest {
 
   @Autowired
   private GameService gameService;
@@ -61,6 +60,33 @@ public class GameServiceIntTests {
     assertEquals(Routes.GAME + Routes.GAME_OUTBOUND, message[1]);
     final WorldChunkMessage worldChunkMessage = JsonUtils.jsonToObject((String) message[2], WorldChunkMessage.class);
     assertFalse(worldChunkMessage.getTiles().isEmpty());
+  }
+
+  @Test(expected = AuthUserDoesNotExistException.class)
+  public void testBuildBuildingUserDoesNotExist() throws Exception {
+    gameService.handleBuildBuildingMessage("dontexist", new BuildBuildingForm("1", 15, 15));
+  }
+
+  @Test(expected = UserDoesNotHaveKingdomException.class)
+  public void testBuildingBuildingNoKingdom() throws Exception {
+    final String username = CommonUtils.generateRandomString(15);
+    register(username);
+    final BuildBuildingForm form = new BuildBuildingForm("1", 15, 15);
+    gameService.handleBuildBuildingMessage(username, form);
+  }
+
+
+  @Test
+  public void testBuildBuilding() throws Exception {
+    final String username = CommonUtils.generateRandomString(15);
+    register(username);
+    createKingdom(username, CommonUtils.generateRandomString(15));
+
+    final BuildBuildingForm form = new BuildBuildingForm(
+      "1", 15, 15
+    );
+
+    gameService.handleBuildBuildingMessage(username, form);
   }
 
 }
