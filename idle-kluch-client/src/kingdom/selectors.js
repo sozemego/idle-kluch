49bbd4d201e7edc5830d1a4692d85ff5b9c7e810
--global <piknypiec@gmail.com>
2018-05-01 21:48:28 +0200

    Fixed display of a list of buildings

--- idle-kluch-client/src/kingdom/selectors.js
+++ idle-kluch-client/src/kingdom/selectors.js
@@ -1,4 +1,5 @@
 import { rootSelector } from "../store/utils";
+import { COMPONENT_TYPES } from "../game/constants";
 
 export const root = rootSelector("kingdom");
 
@@ -7,5 +8,20 @@ export const hasKingdom = state => !!getKingdom(state);
 export const shouldShowCreateKingdomForm = state => root(state).showCreateKingdomForm;
 export const getKingdomNameRegistrationError = state => root(state).kingdomNameRegistrationError;
 
-export const getConstructableBuildings = state => root(state).constructableBuildings;
+export const getConstructableBuildings = state => {
+  const buildings  = root(state).constructableBuildings;
+
+  return buildings.map(building => {
+    const nameComponent = getNameComponent(building);
+    return {
+      id: building.id,
+      name: nameComponent.name,
+    }
+  })
+};
+
+const getNameComponent = (building) => {
+  return building.components.find(comp => comp.componentType === COMPONENT_TYPES.NAME);
+};
+
 export const getSelectedConstructableBuilding = state => root(state).selectedConstructableBuilding;
