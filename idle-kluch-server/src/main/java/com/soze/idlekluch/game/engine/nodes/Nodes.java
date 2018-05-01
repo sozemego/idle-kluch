7064d9b52ef91126b179f218307afb0196e9367b
--global <piknypiec@gmail.com>
2018-05-01 18:07:25 +0200

    Added OCCUPY_SPACE Node

--- idle-kluch-server/src/main/java/com/soze/idlekluch/game/engine/nodes/Nodes.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/game/engine/nodes/Nodes.java
@@ -2,11 +2,14 @@ package com.soze.idlekluch.game.engine.nodes;
 
 import com.soze.idlekluch.game.engine.components.OwnershipComponent;
 import com.soze.idlekluch.game.engine.components.PhysicsComponent;
+import com.soze.idlekluch.game.engine.components.StaticOccupySpaceComponent;
 import com.soze.klecs.node.Node;
 
 public interface Nodes {
 
   Node PHYSICAL_NODE = Node.of(PhysicsComponent.class);
   Node OWNERSHIP = Node.of(OwnershipComponent.class);
+
+  Node OCCUPY_SPACE = Node.of(PhysicsComponent.class, StaticOccupySpaceComponent.class);
 
 }
