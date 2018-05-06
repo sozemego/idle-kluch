62a0145785692b4feb13eaac2d1de1b6ec0dfc5a
--global <piknypiec@gmail.com>
2018-05-06 21:36:32 +0200

    Added TODO

--- idle-kluch-server/src/main/java/com/soze/idlekluch/utils/jpa/QueryUtils.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/utils/jpa/QueryUtils.java
@@ -10,6 +10,7 @@ public class QueryUtils {
       final Object result = query.getSingleResult();
       return Optional.of(clazz.cast(result));
     } catch (Exception e) {
+      //TODO find which exceptions can be safely ignored and which have to be rethrown
       return Optional.empty();
     }
   }
