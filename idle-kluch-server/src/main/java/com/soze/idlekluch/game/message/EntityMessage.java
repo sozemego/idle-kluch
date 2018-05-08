15a2e0a121d9da1d1f2dac0d32080a19acfb7af6
--global <piknypiec@gmail.com>
2018-05-08 16:00:06 +0200

    Jackson annotations for EntityMessage

--- idle-kluch-server/src/main/java/com/soze/idlekluch/game/message/EntityMessage.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/game/message/EntityMessage.java
@@ -1,6 +1,8 @@
 package com.soze.idlekluch.game.message;
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.soze.idlekluch.game.engine.components.BaseComponent;
 import com.soze.idlekluch.utils.jpa.EntityUUID;
 
@@ -9,23 +11,37 @@ import java.util.Objects;
 
 public class EntityMessage extends OutgoingMessage {
 
-  @JsonUnwrapped
-  private final EntityUUID id;
-
   private final List<BaseComponent> components;
 
-  public EntityMessage(final EntityUUID id, final List<BaseComponent> components) {
+  private final EntityUUID entityId;
+
+  public EntityMessage(final EntityUUID entityId,
+                       final List<BaseComponent> components) {
     super(OutgoingMessageType.ENTITY);
-    this.id = Objects.requireNonNull(id);
+    this.entityId = Objects.requireNonNull(entityId);
     this.components = Objects.requireNonNull(components);
   }
 
+  @JsonCreator
+  public static EntityMessage create(@JsonProperty("id") final EntityUUID id,
+                                     @JsonProperty("components") final List<BaseComponent> components) {
+    return new EntityMessage(id, components);
+  }
+
+  @JsonGetter(value = "id")
   public EntityUUID getId() {
-    return id;
+    return entityId;
   }
 
   public List<BaseComponent> getComponents() {
     return components;
   }
 
+  @Override
+  public String toString() {
+    return "EntityMessage{" +
+             "entityId=" + entityId +
+             ", components=" + components +
+             '}';
+  }
 }
