326b7d7d66090797544acbd73f66105427214f17
--global <piknypiec@gmail.com>
2018-05-15 13:12:18 +0200

    Fixed comment

--- idle-kluch-client/src/ecs/utils.js
+++ idle-kluch-client/src/ecs/utils.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 /**
  * Tries to find a component in an entity json.
- * This is Entity class, but a json representation send from backend.
+ * This is not for use with Entity class, but a json representation send from backend.
  */
 export const findComponent = (entity, componentType) => {
   const components = _.get(entity, 'components', []);
