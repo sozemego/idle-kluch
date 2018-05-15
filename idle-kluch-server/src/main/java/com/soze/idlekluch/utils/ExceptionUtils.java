d03c0a55e814ceb7e12f0114137740392018ba02
--global <piknypiec@gmail.com>
2018-05-15 21:03:04 +0200

    Small refactor

--- idle-kluch-server/src/main/java/com/soze/idlekluch/utils/ExceptionUtils.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/utils/ExceptionUtils.java
@@ -3,17 +3,21 @@ package com.soze.idlekluch.utils;
 import com.soze.idlekluch.utils.http.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Objects;
+
 public class ExceptionUtils {
 
   /**
    * Converts a {@link ErrorResponse} from the application into a {@link ResponseEntity}.
+   *
    * @param errorResponse response to convert
-   * @return JAX-RS response
+   * @return {@link ResponseEntity}
    */
-  public static ResponseEntity convertErrorResponse(ErrorResponse errorResponse) {
+  public static ResponseEntity convertErrorResponse(final ErrorResponse errorResponse) {
+    Objects.requireNonNull(errorResponse);
     return ResponseEntity
-      .status(errorResponse.getStatusCode())
-      .body(JsonUtils.objectToJson(errorResponse));
+               .status(errorResponse.getStatusCode())
+               .body(JsonUtils.objectToJson(errorResponse));
   }
 
 }
