6588441d877004302f9002ddddf582c8e0538c64
--global <piknypiec@gmail.com>
2018-05-16 12:16:04 +0200

    Alt text for an image

--- idle-kluch-client/src/game/menu/Toolbar.js
+++ idle-kluch-client/src/game/menu/Toolbar.js
@@ -18,7 +18,7 @@ class Toolbar extends Component {
     return (
       <div className={styles.container}>
         <div className={styles.idle_bucks_container}>
-          <img src={idleBuck} className={styles.idle_buck_img}/>
+          <img src={idleBuck} className={styles.idle_buck_img} alt={"Idle bucks coin"}/>
           <span className={styles.idle_buck_amount}>{idleBucks}</span>
         </div>
       </div>
