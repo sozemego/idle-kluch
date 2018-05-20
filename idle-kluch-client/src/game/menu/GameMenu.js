4a5005570152538c3c4680e8b8d203bcb50c9c29
--global <piknypiec@gmail.com>
2018-05-20 19:03:15 +0200

    Fixed a React issue

--- idle-kluch-client/src/game/menu/GameMenu.js
+++ idle-kluch-client/src/game/menu/GameMenu.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import * as gameActions from "../actions";
@@ -29,10 +29,12 @@ class GameMenu extends Component {
   render() {
     const { getAlreadyConnectedDialog } = this;
 
-    return [
-      <BuildingList />,
-      getAlreadyConnectedDialog(),
-    ];
+    return (
+      <Fragment>
+        <BuildingList/>
+        {getAlreadyConnectedDialog()}
+      </Fragment>
+    );
   }
 }
 
