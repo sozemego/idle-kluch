3b54e30edb5462acc0b7b87c2fa545ea563260aa
--global <piknypiec@gmail.com>
2018-05-20 18:34:22 +0200

    Bug fix

--- idle-kluch-server/src/main/java/com/soze/idlekluch/kingdom/service/KingdomServiceImpl.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/kingdom/service/KingdomServiceImpl.java
@@ -15,6 +15,7 @@ import com.soze.idlekluch.utils.PoissonDiscSampler;
 import com.soze.idlekluch.utils.jpa.EntityUUID;
 import com.soze.idlekluch.world.entity.TileId;
 import com.soze.idlekluch.world.service.WorldService;
+import com.soze.idlekluch.world.utils.WorldUtils;
 import com.soze.klecs.entity.Entity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -88,6 +89,7 @@ public class KingdomServiceImpl implements KingdomService {
   }
 
   @Override
+  @Transactional
   public void removeKingdom(final String owner) {
     Objects.requireNonNull(owner);
     LOG.info("User [{}] is trying to remove their kingdom", owner);
@@ -132,12 +134,10 @@ public class KingdomServiceImpl implements KingdomService {
     //2. Get all their positions and translate them to coordinates of tiles
     final List<Point> points = buildings
                                  .stream()
-                                 .map(building -> {
-                                   final PhysicsComponent physicsComponent = building.getComponent(PhysicsComponent.class);
-                                   final float x = physicsComponent.getX();
-                                   final float y = physicsComponent.getY();
-                                   return new Point((int) x / WorldService.TILE_SIZE, (int) y / WorldService.TILE_SIZE);
-                                 })
+                                 .map(WorldUtils::getEntityTileId)
+                                 .filter(Optional::isPresent)
+                                 .map(Optional::get)
+                                 .map(tileId -> new Point(tileId.getX(), tileId.getY()))
                                  .collect(Collectors.toList());
 
     //3. We also need to get all kingdom starting points. A player may have zero buildings,
