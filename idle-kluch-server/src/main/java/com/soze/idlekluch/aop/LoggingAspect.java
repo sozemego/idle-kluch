c1be0b226dee8d63cebdc9b99727cb4bb76b40ea
--global <piknypiec@gmail.com>
2018-05-22 17:18:21 +0200

    Improved Authorized logging a little

--- idle-kluch-server/src/main/java/com/soze/idlekluch/aop/LoggingAspect.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/aop/LoggingAspect.java
@@ -1,9 +1,7 @@
 package com.soze.idlekluch.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,10 +20,31 @@ public class LoggingAspect {
   public void logAuthorizedMethod(final JoinPoint pjp) throws Throwable {
     final String username = SecurityContextHolder.getContext().getAuthentication().getName();
     LOG.info(
-      "AUTH: [{}] [{}] [{}]",
+      "AUTH BEFORE: [{}] [{}] [{}]",
       username,
       pjp.getSignature().toShortString(),
       pjp.getArgs()
+    );
+  }
+
+  @AfterReturning("authorizedMethodExecution()")
+  public void afterAuthorizedMethodReturning(final JoinPoint pjp) throws Throwable {
+    final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    LOG.info(
+      "AUTH AFTER: [{}] [{}]",
+      username,
+      pjp.getSignature().toShortString()
+    );
+  }
+
+  @AfterThrowing(pointcut = "authorizedMethodExecution()", throwing = "ex")
+  public void afterAuthorizedMethodThrowing(final JoinPoint pjp, final Throwable ex) throws Throwable {
+    final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    LOG.info(
+      "AUTH THROWN: [{}] [{}] [{}]",
+      username,
+      pjp.getSignature().toShortString(),
+      ex
     );
   }
 
