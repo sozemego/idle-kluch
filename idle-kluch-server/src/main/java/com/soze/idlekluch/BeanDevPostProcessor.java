b514757b166080f4c3aea934119c4d9a53c0985a
--global <piknypiec@gmail.com>
2018-05-22 16:30:06 +0200

    Logging statement for after bean construction

--- idle-kluch-server/src/main/java/com/soze/idlekluch/BeanDevPostProcessor.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/BeanDevPostProcessor.java
@@ -1,5 +1,7 @@
 package com.soze.idlekluch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
  * Used for development.
  */
 public class BeanDevPostProcessor implements BeanPostProcessor {
+
+  private static final Logger LOG = LoggerFactory.getLogger(BeanDevPostProcessor.class);
 
   @Override
   public Object postProcessBeforeInitialization(final Object bean, final String beanName) throws BeansException {
@@ -17,4 +21,9 @@ public class BeanDevPostProcessor implements BeanPostProcessor {
     return bean;
   }
 
+  @Override
+  public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
+    LOG.info("[{}] initialized", beanName);
+    return bean;
+  }
 }
