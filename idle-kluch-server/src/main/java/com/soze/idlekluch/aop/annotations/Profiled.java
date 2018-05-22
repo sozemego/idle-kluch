097b3e2b19742407e6ff2259cb695fd655dc1261
--global <piknypiec@gmail.com>
2018-05-22 16:56:09 +0200

    Comment fix

--- idle-kluch-server/src/main/java/com/soze/idlekluch/aop/annotations/Profiled.java
+++ idle-kluch-server/src/main/java/com/soze/idlekluch/aop/annotations/Profiled.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation used to mark methods as profiled, their execution time will be measured by .
+ * Annotation used to mark methods as profiled, their execution time will be measured.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
